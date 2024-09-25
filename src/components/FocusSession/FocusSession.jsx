2279ea324dac27f87efb6e3d4a75537edb496896
maishamir <45680002+maishamir@users.noreply.github.com>
2024-09-25 02:51:28 -0700

    [WIP] focus reset to placeholder on empty input

--- src/components/FocusSession/FocusSession.jsx
+++ src/components/FocusSession/FocusSession.jsx
@@ -1,22 +1,66 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import "./FocusSession.scss";
 
 function FocusSession() {
   const [focus, setFocus] = useState("What's your focus for this session?");
+  const [isEditable, setIsEditable] = useState(false);
+  const focusRef = useRef(null);
+  const hasCleared = useRef(false);
 
-  function handleFocusChange(e) {
-      e.target.contentEditable = true;
+  function handleFocusEdit(e) {
+    e.target.contentEditable = true;
+    setIsEditable(true);
   }
-    
-    function handleSetFocus(e) {
-        if (e.key === "Enter") {
-            e.target.contentEditable = false;
-            setFocus(e.target.innerHTML)
-        }
+
+  function handleBlur(e) {
+    setIsEditable(false)
+    e.target.contentEditable = false;
+
+    if (e.target.innerHTML === "") {
+      setFocus("What's focus for this session?")
     }
+    
+  }
+  // useEffect(() => {
+  //   if (isEditable) {
+  //     const handleClickOutside = (e) => {
+  //       if (focusRef.current && !focusRef.current.contains(e.target)) {
+  //         setIsEditable(false);
+  //       }
+  //     };
+
+  //     document.addEventListener("click", handleClickOutside);
+  //     return () => document.removeEventListener("click", handleClickOutside);
+  //   }
+  // }, [isEditable]);
+
+  // sets the focus to whatever the user inputs upon hitting enter; clears the focus before typing
+  function handleSetFocus(e) {
+    if (!hasCleared.current) {
+      focusRef.current.innerText = "";
+      hasCleared.current = true;
+    }
+    if (e.key === "Enter") {
+      if (focusRef.current.value === "") {
+        setFocus("What's your focus for this session?");
+      } else {
+        e.target.contentEditable = false;
+        setFocus(e.target.innerHTML);
+        setIsEditable(false);
+      }
+    }
+  }
   return (
-    <div>
-      <h1 onClick={handleFocusChange} onChange={e => setFocus(e.target.value)} onKeyDown={handleSetFocus}>{focus}</h1>
-      <button></button>
+    <div className="focusSession">
+      <h1
+        ref={focusRef}
+        className={`focusSession__focus ${isEditable ? "edit" : ""}`}
+        onClick={handleFocusEdit}
+        onKeyDown={handleSetFocus}
+        onBlur={handleBlur}
+      >
+        {focus}
+      </h1>
     </div>
   );
 }
