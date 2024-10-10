3cd0fc578754a452f9ce2ff13d7dc770c5d107e8
maishamir <45680002+maishamir@users.noreply.github.com>
2024-10-10 13:52:20 -0700

    refactoring app into individual components; added completion screen

---
+++ src/components/CompletionScreen/CompletionScreen.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function CompletionScreen() {
+  return (
+      <div>
+          <h1>Great job! What do you want to do next?</h1>
+
+          <button>Short break</button>
+          <button>Long break</button>
+          <button>New Session</button>
+    </div>
+  )
+}
+
+export default CompletionScreen
