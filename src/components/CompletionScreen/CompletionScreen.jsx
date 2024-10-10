e4af8992637c77a9353ce8ad3321637b6fdb8216
maishamir <45680002+maishamir@users.noreply.github.com>
2024-10-10 14:08:16 -0700

    basic styled screen for session completion

--- src/components/CompletionScreen/CompletionScreen.jsx
+++ src/components/CompletionScreen/CompletionScreen.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./CompletionScreen.scss";
 
 function CompletionScreen() {
   return (
-      <div>
-          <h1>Great job! What do you want to do next?</h1>
+    <div className="completionScreen">
+      <h1 className="completionScreen__title">Great job!</h1>
+      <h2 className="completionScreen__subtitle">What's next?</h2>
 
-          <button>Short break</button>
-          <button>Long break</button>
-          <button>New Session</button>
+      <div className="completionScreen__buttons">
+        <button className="completionScreen__button">Short break</button>
+        <button className="completionScreen__button">Long break</button>
+        <button className="completionScreen__button">New Session</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CompletionScreen
+export default CompletionScreen;
