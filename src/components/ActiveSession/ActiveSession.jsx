fafbb195adb9ea46b6140ebd65e82675b08c9b30
maishamir <45680002+maishamir@users.noreply.github.com>
2024-11-09 22:14:44 -0800

    style session count; added attributions file

--- src/components/ActiveSession/ActiveSession.jsx
+++ src/components/ActiveSession/ActiveSession.jsx
@@ -6,17 +6,15 @@ import useTimer from "../../hooks/useTimer";
 import EditIcon from "@mui/icons-material/Edit";
 import { useNavigate } from "react-router-dom";
 import TimerButtons from "../TimerButtons/TimerButtons";
-
+import flameIcon from "../../assets/fire.png"
 
 function ActiveSession() {
-
   let navigate = useNavigate();
   const [input, setInput] = useState();
 
   const [focusSession, setFocusSession] = useState("");
   const [isEditing, setIsEditing] = useState(false);
   const [sessions, setSessions] = useState(0);
-
 
   const inputRef = useRef(null);
 
@@ -48,13 +46,12 @@ function ActiveSession() {
 
   useEffect(() => {
     if (minutes === 0 && seconds === 0) {
-
       const updateCount = sessions + 1;
-      setSessions(updateCount)
+      setSessions(updateCount);
 
-      localStorage.setItem("sessionCount", updateCount)
+      localStorage.setItem("sessionCount", updateCount);
       const delay = setTimeout(() => {
-        navigate("/complete")
+        navigate("/complete");
       }, 2800);
 
       return () => clearTimeout(delay);
@@ -64,54 +61,60 @@ function ActiveSession() {
   // to get the saved sessionCount from localStorage
   useEffect(() => {
     if ("sessionCount" in { ...localStorage }) {
-      const countNumber = Number.parseInt(localStorage.getItem("sessionCount"))
-      setSessions(countNumber)
+      const countNumber = Number.parseInt(localStorage.getItem("sessionCount"));
+      setSessions(countNumber);
     }
-  }, [])
+  }, []);
 
   return (
-    <main className="app">
-      <div className="app__focus">
+    <main className="activeSession">
+      <div className="activeSession__focus">
         <p
-          className={`app__editIcon ${
-            isEditing ? "app__editIcon--active" : ""
-            }`}
+          className={`activeSession__editIcon ${
+            isEditing ? "activeSession__editIcon--active" : ""
+          }`}
           style={{ opacity: isEditing && 1 }}
         >
           <EditIcon fontSize="medium" />
         </p>
 
-        <div className="app_setFocus">
+        <div className="activeSession_setFocus">
           {isEditing ? (
             <input
               type="text"
               placeholder={focusSession === "" ? "Click to set a focus" : ""}
               ref={inputRef}
-              className="app__focusInput"
+              className="activeSession__focusInput"
               onChange={handleSetFocus}
               onBlur={handleBlur}
               value={focusSession}
               maxLength="40"
-              onKeyDown={(e) => { if (e.key === "Enter") handleBlur() }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") handleBlur();
+              }}
             />
           ) : (
-            <h1 className="app__focusSession" onClick={handleClick} >
+            <h1 className="activeSession__focusSession" onClick={handleClick}>
               {focusSession || "Click to set a focus"}
             </h1>
           )}
         </div>
       </div>
-      <div className="app__timerDisplay">
+      <div className="activeSession__timerDisplay">
         <TimerDisplay minutes={minutes} seconds={seconds} />
-        <TimerButtons input={input}
+        <TimerButtons
+          input={input}
           setInput={setInput}
           buttonText={buttonText}
           timeEditable={timeEditable}
           handleStartStop={handleStartStop}
-          handleClear={handleClear} 
+          handleClear={handleClear}
           skip={false}
-          />
-        <p>{sessions}</p>
+        />
+        <div className="activeSession__sessionCounter">
+          <img src={flameIcon} alt="" className="activeSession__flameIcon" />
+          <p> {sessions}</p>
+        </div>
       </div>
     </main>
   );
